e2e07e90611f28d9863a2eb71b52bb0534ef74a4
Saga <diegotorres1106@gmail.com>
2015-11-25 23:14:11 -0500

    Draggable and rezisable rectangle using label

---
+++ LabelRectangle/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace LabelRectangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.insertRectangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertRectangleToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(159, 26);
+            // 
+            // insertRectangleToolStripMenuItem
+            // 
+            this.insertRectangleToolStripMenuItem.Name = "insertRectangleToolStripMenuItem";
+            this.insertRectangleToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.insertRectangleToolStripMenuItem.Text = "Insert Rectangle";
+            this.insertRectangleToolStripMenuItem.Click += new System.EventHandler(this.insertRectangleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 358);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem insertRectangleToolStripMenuItem;
+    }
+}
+
---
+++ LabelRectangle/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LabelRectangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(this, new Point(e.X, e.Y));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(@"D:\xp_wallpaper.jpg");
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            e.Graphics.DrawImage(bitmap, new Point(0, 0));
+        }
+
+        private void insertRectangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var lblRectangle = new Label
+            {
+                BackColor = Color.FromArgb(20, Color.Red.R, Color.Red.G, Color.Red.B),
+                BorderStyle  = BorderStyle.FixedSingle,
+                Location = PointToClient(MousePosition),
+                Size = new Size(40, 40)
+            };
+
+            lblRectangle.MouseDown += new MouseEventHandler(lblRectangle_MouseDown);
+            lblRectangle.MouseMove += new MouseEventHandler(lblRectangle_MouseMove);
+            lblRectangle.MouseUp += new MouseEventHandler(lblRectangle_MouseUp);
+
+            Controls.Add(lblRectangle);
+        }
+
+        private void lblRectangle_MouseUp(object sender, MouseEventArgs e)
+        {
+            _isResizing = false;
+            _isDragging = false;
+        }
+
+        private void lblRectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control control = (Control)sender;
+
+            if (((e.X + 5) > control.Width) && ((e.Y + 5) > control.Height))
+            {
+                control.Cursor = Cursors.SizeNWSE;
+            }
+            else
+            {
+                control.Cursor = Cursors.Arrow;
+            }
+        }
+
+        private void lblRectangle_MouseDown(object sender, MouseEventArgs e)
+        {
+            Control control = (Control) sender;
+
+            if (e.X > control.Width && e.Y > control.Height)
+            {
+                _isResizing = true;
+            }
+            else
+            {
+                _isDragging = true;
+            }
+        }
+
+        private bool _isResizing;
+        private bool _isDragging;
+    }
+}
